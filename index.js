d34574dc4492aac6e48af323e6440377bff36bc2
Nguyen Duy Cong <duycong.cn@gmail.com>
2020-10-16 19:15:53 +0700

    show image

--- index.js
+++ index.js
@@ -1,24 +1,43 @@
 $(document).ready(function () {
 
     var textInputFile = $('#text-input-file');
-    var readFile = $('#read-file');
     var btnGetFile = $('#get-input-file');
-    
+    var readFile = $('#read-file');
+
     var textRenameFile = $('#text-rename-file');
     var btnRename = $('#btn-rename');
     
     // bắt sự kiện cho button btnGetFile
     btnGetFile.click(function () {
         readFile.click();
+        if ($("img").length) {
+            let images = $("img");
+            images.remove();
+        }
     });
     
     btnGetFile.mouseleave(function () {
         if (readFile.val()) {
             var value = readFile.val();
-            console.log(typeof value);
             textInputFile.val(value);
+            console.log(value);
         }
     });
+
+    // show ảnh
+    $('#read-file').change(function (e) {
+        for (var i = 0; i < e.originalEvent.srcElement.files.length; i++) {
+
+            var file = e.originalEvent.srcElement.files[i];
     
-    
+            var img = document.createElement("img");
+            var reader = new FileReader();
+            reader.onloadend = function() {
+                 img.src = reader.result;
+            }
+            reader.readAsDataURL(file);
+            $("#view").after(img);
+        };
+    });
+
 });
